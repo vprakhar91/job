3f778d84224f85ff60f1329cfcc3c3dfb5aeacdf
Prakhar Verma <46259033+vprakhar91@users.noreply.github.com>
2024-09-06 16:09:50 +0530

    Update Footer.jsx

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -7,6 +7,11 @@ const Footer = ({ url }) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.likeBtn}>
+         <Image
+          source={icons.heartOutline}
+          resizeMode='contain'
+          style={styles.likeBtnImage}
+        />
       </TouchableOpacity>
 
       <TouchableOpacity
